2030ef47a350cb8b5ec42316cc6763cf18978b5d
J0N98 <jon@kuep-pers.de>
2026-05-08 10:15:59 +0200

    Logging configuration to save in folder

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 import os
 import logging
 logging.basicConfig(
-    filename="Dashboard.log",
+    filename="/logs/Dashboard.log",
     format="%(asctime)s | %(name)s | %(levelname)s | %(message)s",
     level=logging.DEBUG,
     force=True
